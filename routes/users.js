d119000983c15bdeee10196368342e1a85849c34
Aaron Chacon <aaronch.dev@gmail.com>
2023-06-23 21:45:12 -0300

    feat: add post and get by id

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,30 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
+
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "Aaron",
-    lastName: "Chacon",
-    age: 31,
-  },
-  {
-    firstName: "Mariam",
-    lastName: "Levy",
-    age: 32,
-  },
-];
+const users = [];
 
 // all routes in here are starting with /users
 router.get("/", (req, res) => {
   console.log(users);
   res.send(users);
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+  const userWithId = { id: uuidv4(), ...user };
+  users.push(userWithId);
+  res.send({
+    info: `User with the name ${user.firstName} add to the database!`,
+    data: user,
+  });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
 });
 
 export default router;
