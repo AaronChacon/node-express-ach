7c914c1af060a3c4b4a88b5a399c3c5c7cdac5da
Aaron Chacon <aaronch.dev@gmail.com>
2023-06-23 22:11:31 -0300

    feat: add delete route

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,10 @@ import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 // all routes in here are starting with /users
 router.get("/", (req, res) => {
-  console.log(users);
   res.send(users);
 });
 
@@ -17,7 +16,7 @@ router.post("/", (req, res) => {
   users.push(userWithId);
   res.send({
     info: `User with the name ${user.firstName} add to the database!`,
-    data: user,
+    data: userWithId,
   });
 });
 
@@ -25,6 +24,17 @@ router.get("/:id", (req, res) => {
   const { id } = req.params;
   const foundUser = users.find((user) => user.id === id);
   res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id !== id);
+
+  res.send({
+    info: `User wast delete to the database!`,
+    data: users,
+  });
 });
 
 export default router;
